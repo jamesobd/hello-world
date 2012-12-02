2b02c8fb4ea92e8bd889d0dfe088df3334f2f4da
James <james@obdstudios.com>
2012-12-02 02:35:36 -0700

    remove onclick for towing page submit

--- phonegap_scripts.js
+++ phonegap_scripts.js
@@ -34,7 +34,7 @@ function onDeviceReady() {
 	// Any pages which have a childbrowser_redirect_url variable will redirect to a ChildBrowser
 	if ( childbrowser_redirect_url != '' ) {
 		alert('ok lets do this');
-		childBrowser(childbrowser_redirect_url);
+		//childBrowser(childbrowser_redirect_url);
 	}
 	else {
 		alert('crap');
