38cee1c6f92c3746c2d40192753003bc3d0756ac
James <james@obdstudios.com>
2012-12-01 23:41:28 -0700

    remove onclick for towing page submit

--- phonegap_scripts.js
+++ phonegap_scripts.js
@@ -1,34 +1,49 @@
+/**
+ * Some scripts to assist in migrating a website to PhoneGap
+ * 
+ */
+
 
 /**
- * Callback for when phonegap is ready
+ * Listener for when phonegap is ready
  */
 $(function() {
-	console.log('onLoad');
-	
 	document.addEventListener("deviceready", onDeviceReady, false);
 });
+
 
 /**
  * DeviceReady event callback
  */
 function onDeviceReady() {
-	console.log('onDeviceReady');
 	
-	// Listener to have all form submissions to use phonegap childbrowser
-	$('form input[type="submit"]').click(function(e) {
-		var params = $(e.target.form).serialize();
-		window.plugins.childBrowser.showWebPage($(e.target.form).attr('action')+'?'+params, {showLocationBar: false, showAddress: false, showNavigationBar: false});
+	// Forms using POST method will not work on PhoneGap.
+	// Need to use ChildBrowser on any links taking us to pages with these forms.
+	if ( $('form[method="post"]') ) {
+		alert('Warning: This page contains a POST form.  Add a childbrowser class to the link that got you here.');
+	}
+	
+	// Any links that have the childbrowser class will use the ChildBrowser plugin
+	$('a.childbrowser').on('click', function(){
 		e.preventDefault();
-		e.stopImmediatePropagation();
-		return false;
+		
+		// Load website version in ChildBrowser instead
+		childBrowser(e.target.href);
+	});
+	
+	// Forms using GET method will work with PhoneGap and the submission sent to childbrowser
+	$('form[method="get"]').on('submit', function(e) {
+		e.preventDefault();
+		var params = $(e.target).serialize();
+		childBrowser(e.target.action+'?'+params);
 	});
 }
 
+
 /**
- * Run after DOM and phonegap are ready
+ * Displays the url in a fullscreen ChildBrowser
  */
-//document.addEventListener("deviceready", onDeviceReady, false);
-
-
-
-
+function childBrowser(url) {
+	url = typeof url !== 'undefined' ? a : window.location;
+	window.plugins.childBrowser.showWebPage(url, {showLocationBar: false, showAddress: false, showNavigationBar: false});
+}
