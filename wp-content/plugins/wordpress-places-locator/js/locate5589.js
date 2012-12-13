5e2b9207f6cc710b0571eaada836d95bbbec5014
James <james@obdstudios.com>
2012-12-13 16:16:27 -0700

    update

--- wp-content/plugins/wordpress-places-locator/js/locate5589.js
+++ wp-content/plugins/wordpress-places-locator/js/locate5589.js
@@ -48,7 +48,7 @@ function removeMessage() {
 function getLocation() {
 	//alert('getLocation'); // jj
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(showPosition,showError, {enableHighAccuracy: true});
+		navigator.geolocation.getCurrentPosition(showPosition,showError, { timeout: 15000, enableHighAccuracy: true });
 //    	foundYouMessage();
 	} else {
    	 	alert("Geolocation is not supported by this browser.");
