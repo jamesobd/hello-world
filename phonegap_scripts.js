fc3107be89800df9c6500ce9568124d2e84a1c0b
James <james@obdstudios.com>
2012-12-13 18:50:18 -0700

    update

--- phonegap_scripts.js
+++ phonegap_scripts.js
@@ -18,7 +18,8 @@ $(function(){
  * DeviceReady event callback
  */
 function onDeviceReady() {
-	
+	alert(window.plugins.childBrowser);
+
 	// Forms using POST method will not work on PhoneGap.
 	// Use a ChildBrowser on any links taking us to pages with these forms.
 	if ( $('form[method="post"]').length > 0 ) {
